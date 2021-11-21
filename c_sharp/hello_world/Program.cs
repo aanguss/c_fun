c6173354a7e1544aae496b7ffd113dac92014078
Anthony Angus <aangus@gmail.com>
2021-11-21 00:18:44 -0800

    updates to c# hello world

---
+++ c_sharp/hello_world/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+// using System;
+//global using global::System;
+//global using global::System.Collections.Generic;
+//global using global::System.IO;
+//global using global::System.Linq;
+//global using global::System.Net.Http;
+//global using global::System.Threading;
+//global using global::System.Threading.Tasks;
+
+Console.WriteLine("Hello, World!");
